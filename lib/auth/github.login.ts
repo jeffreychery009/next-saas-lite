9ecae87bb0e0d678da9833419df47359bb4a4901
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-04 04:43:45 -0400

    Created separate google and github login functions

---
+++ lib/auth/github.login.ts
@@ -0,0 +1,13 @@
+import { supabase } from "../supabaseBrowser";
+
+export const handleGithubLogin = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
+    },
+  });
+  if (error) {
+    console.error("Error signing in with Github", error);
+  }
+};
---
+++ lib/auth/google.login.ts
@@ -0,0 +1,13 @@
+import { supabase } from "@/lib/supabaseBrowser";
+
+export const handleGoogleLogin = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${window.location.origin}/auth/callback`,
+    },
+  });
+  if (error) {
+    console.error("Error signing in with Google", error);
+  }
+};
