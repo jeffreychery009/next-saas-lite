caded26b38e4146886ed4faa831c664c26167498
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-05 00:55:39 -0400

    Removed unused imports

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,22 +1,15 @@
-"use client"
+'use client';
 
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import { supabase } from '@/lib/supabaseBrowser'
-import { redirect, useRouter } from 'next/navigation'
+import React from 'react';
 
-
+import Header from '@/components/header/header';
 
 const page = () => {
-  const handleLogout = () => {
-    supabase.auth.signOut();
-    redirect('/login');
-  }
   return (
-    <div>
-      <Button onClick={handleLogout}>Logout</Button> 
+    <div className="">
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
