34c91c9a9a65da65fccc75804c1e73ab31c49777
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-04 23:32:14 -0400

    Defined layout for main app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,21 +1,23 @@
-import { AppSidebar } from '@/components/sidebar/appSidebar'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import React from 'react'
-
+import { AppSidebar } from '@/components/sidebar/appSidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import Header from '@/components/header/header';
+import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-        <SidebarProvider>
-            
-            <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                {children}
-            </main>
-        </SidebarProvider>
-    </div>
-  )
-}
+    <div className="w-full">
+      <SidebarProvider>
+        <div className="mt-4"></div>
+        <main className="flex w-full flex-row">
+          <AppSidebar />
 
-export default Layout
+          <div className="m-2 flex-1 w-full min-w-0 border-2 border-sidebar-border bg-sidebar rounded-lg">
+            {children}
+          </div>
+        </main>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default Layout;
