3b73cc9707589f13b8ec94d3a9cb9a08495a8099
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-05 15:48:40 -0400

    Adjusted the margin and added section cards

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -3,11 +3,15 @@
 import React from 'react';
 
 import Header from '@/components/header/header';
+import { SectionCards } from '@/components/cards/sectionCards';
 
 const page = () => {
   return (
     <div className="">
       <Header />
+      <div className="sm:m-4 my-4">
+        <SectionCards />
+      </div>
     </div>
   );
 };
