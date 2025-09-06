650251c5edf39519fe6ae9a7ab9ae13adee25f2f
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-06 18:02:51 -0400

    Defined layout for team-name avatar-card

--- app/(root)/settings/page.tsx
+++ app/(root)/settings/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '@/components/header/header';
+import TeamName from '@/components/settings/team-name';
+import AvatarCard from '@/components/settings/avatar-card';
 
 const Page = () => {
   return (
     <div>
       <div>
         <Header />
+      </div>
+      <div className="sm:m-4 my-4 lg:px-6 lg:my-10 px-4">
+        <TeamName />
+        <AvatarCard />
       </div>
     </div>
   );
