d531fedca774f01010f69352a473ee37595063e2
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-06 15:19:18 -0400

    Added header to settings page

---
+++ app/(root)/settings/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/components/header/header';
+
+const Page = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
