6886246af32bf995b96cefda7a138c6e15c39525
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-06 04:20:16 -0400

    Added newly created components and set grid styling

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -5,12 +5,18 @@ import React from 'react';
 import Header from '@/components/header/header';
 import { SectionCards } from '@/components/cards/sectionCards';
 
+import BarChartComponent from '@/components/charts/bar-chart';
+import LineChartComponent from '@/components/charts/line-chart';
 const page = () => {
   return (
     <div className="">
       <Header />
       <div className="sm:m-4 my-4">
         <SectionCards />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:px-6 lg:my-10 px-4 my-4">
+          <BarChartComponent />
+          <LineChartComponent />
+        </div>
       </div>
     </div>
   );
