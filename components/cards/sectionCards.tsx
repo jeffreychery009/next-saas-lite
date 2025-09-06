62dabcdf4e642a3f098a55fab37d26c08ba9eff7
Jeffrey Chery <jeffchery009@gmail.com>
2025-09-06 19:37:16 -0400

    Fixed styling and layout issues

--- components/cards/sectionCards.tsx
+++ components/cards/sectionCards.tsx
@@ -12,24 +12,22 @@ import {
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6">
-      <Card className="w-full">
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+      <Card className="@container/card">
         <CardHeader>
           <CardDescription>Total Revenue</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             $1,250.00
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <TrendingUp />
+            <Badge className=" md:hidden xl:block " variant="outline">
+              <TrendingUp className="md:hidden" />
               +12.5%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <TrendingUp className="size-4" />
-          </div>
+          <div className="line-clamp-1 flex gap-2 font-medium">Trending up this month</div>
           <div className="text-muted-foreground">Visitors for the last 6 months</div>
         </CardFooter>
       </Card>
@@ -40,16 +38,14 @@ export function SectionCards() {
             1,234
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <TrendingDown />
+            <Badge className="md:hidden xl:block" variant="outline">
+              <TrendingDown className="md:hidden" />
               -20%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Down 20% this period <TrendingDown className="size-4" />
-          </div>
+          <div className="line-clamp-1 flex gap-2 font-medium">Down 20% this period</div>
           <div className="text-muted-foreground">Acquisition needs attention</div>
         </CardFooter>
       </Card>
@@ -60,16 +56,14 @@ export function SectionCards() {
             45,678
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <TrendingUp />
+            <Badge className="md:hidden xl:block" variant="outline">
+              <TrendingUp className="md:hidden" />
               +12.5%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Strong user retention <TrendingUp className="size-4" />
-          </div>
+          <div className="line-clamp-1 flex gap-2 font-medium">Strong user retention</div>
           <div className="text-muted-foreground">Engagement exceed targets</div>
         </CardFooter>
       </Card>
@@ -80,16 +74,14 @@ export function SectionCards() {
             4.5%
           </CardTitle>
           <CardAction>
-            <Badge variant="outline">
-              <TrendingUp />
+            <Badge className="md:hidden xl:block" variant="outline">
+              <TrendingUp className="md:hidden" />
               +4.5%
             </Badge>
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Steady performance increase <TrendingUp className="size-4" />
-          </div>
+          <div className="line-clamp-1 flex gap-2 font-medium">Steady performance increase</div>
           <div className="text-muted-foreground">Meets growth projections</div>
         </CardFooter>
       </Card>
